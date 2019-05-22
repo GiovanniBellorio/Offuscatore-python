c277903dcd7ac92089cb1e6240cf2661837f9ce6
Alessandro Cosma <alessandrocosma2@gmail.com>
2019-05-22 18:27:15 +0200

    Update prova.py
    Aggiunta una definizione di classe

--- src/prova.py
+++ src/prova.py
@@ -32,3 +32,21 @@ print(b)
 b = [{"key":"value"},{"key1":"value1"}]
 print(b)
 
+
+class Employee:
+    'Common base class for all employees'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
+
+emp1 = Employee("Zara", 2000)
+emp1.displayEmployee()
