9070d3498b35ac9682f77f4d2215d432ec651232
KevCosta <kevinc95@hotmail.it>
2019-05-26 18:58:12 +0200

    Example file to test

---
+++ src/prova.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from random import randint
+from random import SystemRandom
+
+g = [36,58,1,46,23,5,16,65,2,41,2,7,1,37,0,11,16,2,21,16]
+
+name = "Kevin"
+somma = 0
+
+
+
+for i in range(0,10):
+	somma += i
+
+if(i < 20):
+	c = 3
+	#i += c
+
+print(name, somma)
+
